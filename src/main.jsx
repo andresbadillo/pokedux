be3a9199b0443df6a93eeaa357e8cb775ccc6447
Andres Badillo <r.andres.badillo@gmail.com>
2024-09-20 08:57:49 -0500

    Middlewares

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,17 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { pokemonsReducer } from './reducers/pokemons.js'
 import { Provider } from 'react-redux'
-import { legacy_createStore as createStore } from 'redux'
+import { applyMiddleware, compose, legacy_createStore as createStore } from 'redux'
+import { logger, featuring, number } from './middlewares/index.js'
 import './index.css'
 import 'antd/dist/reset.css';
 
-const store = createStore(
-  pokemonsReducer, 
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const composedEnhancers = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logger, featuring, number)
 );
+
+const store = createStore(pokemonsReducer, composedEnhancers);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
